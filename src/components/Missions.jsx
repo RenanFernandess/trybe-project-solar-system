52c8b9b72f9411a348107d6ecdfbd1714a6ba07a
Renan Fernandes <renzinestuods@gmail.com>
2022-07-20 18:34:38 -0300

    REQUISITO-8: Renderiza o componente Title dentro do componente Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import './Missions.css';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <section data-testid="missions"></section>
+      <section data-testid="missions">
+        <Title headline="Missões" />
+      </section>
     );
   }
 }

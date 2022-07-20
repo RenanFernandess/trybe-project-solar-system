436049ef8c2c1d2e786286057f3dd1155c774282
Renan Fernandes <renzinestuods@gmail.com>
2022-07-20 18:55:38 -0300

    REQUISITO-9: Cria um componente chamado MissionCard

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './MissionCard.css';
+import propTypes from 'prop-types';
+
+class MissionCard extends Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+
+    return (
+      <section data-testid="mission-card">
+        <h3 data-testid="mission-name">{ name }</h3>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
+      </section>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: propTypes.string.isRequired,
+  year: propTypes.string.isRequired,
+  country: propTypes.string.isRequired,
+  destination: propTypes.string.isRequired,
+};
+
+export default MissionCard;

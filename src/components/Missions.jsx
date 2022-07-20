7ebb9b31c5b1233806a1e8ad5be30000845a6e2c
Renan Fernandes <renzinestuods@gmail.com>
2022-07-20 18:28:25 -0300

    REQUISITO-7: Cria um componente chamado Missions

---
+++ src/components/Missions.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <section data-testid="missions"></section>
+    );
+  }
+}
+
+export default Missions;
